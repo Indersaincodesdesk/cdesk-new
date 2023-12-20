cb08b05a1cf290c6897b43e314dcbad3ee7a85eb
Indersaincodesdesk <inder.codesdesk@gmail.com>
2023-12-20 16:52:58 +0530

    header design

--- src/assets/js/custom-js.js
+++ src/assets/js/custom-js.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+$(document).ready(function() {
+    $(document).on("click","#toggle", function() {
+        $(this).toggleClass("on");
+        $("#menu").slideToggle();
+    });
+
+    $(document).on("click",".tab-heading", function() {
+        $(this).toggleClass("on");
+        $(".tabcontent").slideToggle();
+    });
+
+});
