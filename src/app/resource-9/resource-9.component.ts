4026b03efa7b94c94070564ff6edca61badec020
Indersaincodesdesk <inder.codesdesk@gmail.com>
2024-01-19 10:27:00 +0530

    blog, footer, homepage , other

--- src/app/resource-9/resource-9.component.ts
+++ src/app/resource-9/resource-9.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./resource-9.component.css']
 })
 export class Resource9Component {
-
+  shareableUrl: string = "http://192.168.1.18:4200/resource"; // Set the URL you want to share
 }
