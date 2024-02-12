cc3f9d44e20ea20395235c1c821fb49cc5519051
loveleen2023 <loveleen@codesdesk.com>
2024-02-12 12:49:27 +0530

    meta tags

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-service',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./service.component.css']
 })
 export class ServiceComponent {
+
+  title = 'Our Services: Expert Shopify Plus Agency for Shopify theme Development & Web Design';
+
+  constructor(
+    private titleServe: Title,
+    private metaServe: Meta
+  ) { 
+   
+  }
+
+  ngOnInit(): void {
+    this.titleServe.setTitle(this.title);
+    this.metaServe.updateTag({name: 'description', content: "Explore our comprehensive services including app development, web designing, Shopify theme development, and e-commerce websites."});
+  }
+
+
   name = "Angular";
   scroll(el: HTMLElement) {
     el.scrollIntoView();
